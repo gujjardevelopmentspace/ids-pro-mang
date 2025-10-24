b417f2f414045b0ed3f3ae42a1203b7d2371a5a8
Yahya developers <yahyagujjar2007@gmail.com>
2025-10-24 14:15:17 +0500

    Add files via upload

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-47da91e0'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.repmmpa6hsg"
+    "revision": "0.bg6af6ub3oo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
